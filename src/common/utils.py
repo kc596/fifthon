79f6bad6c7d1153fdb370f05a450bfe89eba5ed9
kc596 <chaudhary.kc.kunal@gmail.com>
2019-03-20 00:12:57 +0530

    Created logging feature

---
+++ src/common/utils.py
@@ -0,0 +1,23 @@
+import logging
+import logging.config
+import os
+import yaml
+
+configuration = ''
+
+def loadConfiguration(configfile='config/config.yaml'):
+	global configuration 					#needed to modify global copy of variable
+	with open(configfile, 'r') as cf:
+		config = yaml.safe_load(cf.read())
+		configuration = config
+	return config
+
+def getLogger(name):
+	createLogFileDirectory()
+	logging.config.dictConfig(configuration['logs'])
+	logger = logging.getLogger(name)
+	logger.debug("logger init")
+	return logger
+
+def createLogFileDirectory():
+	os.makedirs(os.path.dirname(str(configuration['logs']['handlers']['file']['filename'])), exist_ok=True)
